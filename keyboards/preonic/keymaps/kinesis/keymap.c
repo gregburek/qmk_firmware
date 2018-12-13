c6bfc29ec30e74e86051260bb7e66e032737ce09
Greg Burek <greg.burek@gmail.com>
2018-12-12 23:22:24 -0800

    Latest

--- keyboards/preonic/keymaps/kinesis/keymap.c
+++ keyboards/preonic/keymaps/kinesis/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |  FN  |   =  | ESC  | ALT  | Back | LGUI |ENTER | SPCE | RGUI |   [  |  ]   |  FN  |
+   * |  FN  |   =  | ESC  | ALT  | LGUI | BACK |ENTER | SPCE | RGUI |   [  |  ]   |  FN  |
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_preonic_grid( \
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-    MO(_FN), EQ_LOCK, KC_ESC,  KC_LALT, KC_BSPC, KC_LGUI, KC_ENT,  KC_SPC,  KC_RGUI, KC_LBRC, KC_RBRC, TT(_FN)  \
+    MO(_FN), KC_EQL,  KC_ESC,  KC_LALT, KC_LGUI, KC_BSPC, KC_ENT,  KC_SPC,  KC_RGUI, KC_LBRC, KC_RBRC, TT(_FN)  \
   ),
 
   /* Fn
